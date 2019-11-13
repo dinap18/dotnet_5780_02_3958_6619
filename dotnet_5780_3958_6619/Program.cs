71a60086e9b7b4b89b92dad77474b582a7f41c7a
Dina Pinchuck <dp18@msn.com>
2019-11-13 14:12:35 +0200

    Add project files.

---
+++ dotnet_5780_3958_6619/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet_5780_3958_6619
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
