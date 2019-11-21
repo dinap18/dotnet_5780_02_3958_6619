17e7dd8e815540ad730582028a8b11b92ab4b81a
evebibas <57711394+evebibas@users.noreply.github.com>
2019-11-21 22:26:53 +0200

    Update HostingUnit.cs

--- dotnet_5780_3958_6619/HostingUnit.cs
+++ dotnet_5780_3958_6619/HostingUnit.cs
@@ -18,6 +18,42 @@ namespace dotnet_5780_3958_6619
         public bool ApproveRequest(GuestRequest guestReq)
         {
 
+             //on passe sur la matrice AH NON PAS BESOIN
+            // si ya une suite de jour qui correspond au jour de la demande 
+            // alors ecrit dans la matrice que cest true 
+            // et ecrit dans la demande que isApproved egal a true
+            // et erenvoie true
+            //si pa tt la demande est executee renvoie false 
+            //un guest request est compose de lengthOfStay , EntryDate , ReleaseDate , IsApproved
+        
+                    if( (Diary[guestReq.EntryDate.Month, guestReq.EntryDate.Day] == true) || (Diary[guestReq.ReleaseDate.Month, guestReq.ReleaseDate.Day] == true))
+                    {
+                            return false;
+                    }
+
+                    for (int k = 0; k < lengthOfStay-1; k++) //Verifie pr tt les jours du milieu du sejour
+                    {
+                            int month;
+                            month = Convert.ToInt32((guestReq.EntryDate.Month));
+                        if (guestReq.EntryDate.Day == 31)
+                        { month++; }
+                        DateTime added = (guestReq.EntryDate.AddDays(1));
+
+                        if (Diary[month, k] == true)
+                            return false;
+                    }
+
+                        for (int b = EntryDate.Day; b < (EntryDate.Day + lengthOfStay-1); b++)
+                         {
+                            int month;
+                                month = Convert.ToInt32((guestReq.EntryDate.Month));
+                                if (b == 31)
+                                        month++;
+
+                            Diary[b,month] = true;
+                         }
+                        guestReq.IsApproved = true;
+                  
             return true;
         }
         public int GetAnnualBusyDays()
